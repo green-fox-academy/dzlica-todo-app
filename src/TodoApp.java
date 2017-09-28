cf718d23bf46e14d4a9f87aa9c6c58b46239d0fa
dzlica <dzlica@gmail.com>
2017-09-28 11:21:24 +0200

    Printed out the usage information

---
+++ src/TodoApp.java
@@ -0,0 +1,19 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TodoApp {
+    public static void main(String [] args) {
+        try {
+            Path myPath = Paths.get( "/Users/lica/GreenFox/dzlica-todo-app/printusage.txt");
+            List<String> lines = Files.readAllLines(myPath);
+            for (int i = 0; i < lines.size(); i++) {
+                System.out.println(lines.get(i));
+            }
+
+        } catch (Exception e) {
+            System.out.println("Unable to read the file!");
+        }
+    }
+}
